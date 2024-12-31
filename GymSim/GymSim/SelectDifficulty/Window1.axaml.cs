b4d5b5adafa8f10bf939a0ce6e8406201acdee2d
hhuyyyyyy13012004 <hoanghuyn98@gmail.com>
2024-12-31 21:23:05 +0700

    Window

---
+++ GymSim/GymSim/SelectDifficulty/Window1.axaml.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using ProjectGymSim.GamePartView;
+
+namespace ProjectGymSim.SelectDifficulty;
+
+public partial class Window1 : Window
+{
+    public Window1()
+    {
+        InitializeComponent();
+
+        // Gán sự kiện Click cho các nút
+        EasyButton.Click += EasyButton_Click;
+        NormalButton.Click += NormalButton_Click;
+        HardButton.Click += HardButton_Click;
+    }
+
+    private void EasyButton_Click(object? sender, RoutedEventArgs e)
+    {
+        OpenGameView(1000);
+    }
+
+    private void NormalButton_Click(object? sender, RoutedEventArgs e)
+    {
+        OpenGameView(500);
+    }
+
+    private void HardButton_Click(object? sender, RoutedEventArgs e)
+    {
+        OpenGameView(200);
+    }
+
+    private void OpenGameView(int delay)
+    {
+        GameView.DelayTime = delay; 
+        var gameView = new GameView();
+        gameView.Show();
+    }
+}
