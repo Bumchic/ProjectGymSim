24f0857c69146ad226114a3e6625ba07f9629e7d
Bumchic <duonganhminh05@gmail.com>
2024-12-31 20:59:11 +0700

    Update MainView.axaml.cs

--- ProjectGymSimWindow/ProjectGymSimWindow/Views/MainView.axaml.cs
+++ ProjectGymSimWindow/ProjectGymSimWindow/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using ProjectGymSim.GamePartView;
 
 namespace ProjectGymSimWindow.Views;
 
@@ -7,5 +8,10 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+    public MainView(int Difficulty) : this()
+    {
+        GameView gameView = new GameView(Difficulty);
+        gameView.Show();
     }
 }
