e3803d0f78c41d9d321136faa8ca3939b0de2cf2
DoThanhLoc2334 <locdothanh233@gmail.com>
2024-12-31 21:29:02 +0700

    Add Menu

---
+++ GymSim/GymSim/Menu/GameWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ProjectGymSim.Menu;
+
+public partial class GameWindow : Window
+{
+    public GameWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ GymSim/GymSim/Menu/MainMenu.axaml.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace ProjectGymSim.Menu;
+
+public partial class MainMenu : Window
+{
+    public MainMenu()
+    {
+        InitializeComponent();
+    }
+    private async void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        var gameWindow = new GameWindow();
+        await gameWindow.ShowDialog(this);
+        this.Close();
+    }
+    
+    private void OnExitClick(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+    private void OnFullScreenClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.FullScreen;
+    }
+
+    private void OnResize800x600Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Normal; 
+        this.Width = 800;
+        this.Height = 600;
+    }
+
+    private void OnResize1024x768Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        this.WindowState = WindowState.Normal; 
+        this.Width = 1024;
+        this.Height = 768;
+    }
+}
